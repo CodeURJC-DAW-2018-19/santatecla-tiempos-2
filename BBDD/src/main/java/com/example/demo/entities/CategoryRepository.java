2e8ffc64530ee7de18798a399007de5e89222192
AzoriusSergh <sergiolidi@gmail.com>
2019-02-26 11:35:00 +0100

    LinkEventCategory
    Categories linked to Events

--- BBDD/src/main/java/com/example/demo/entities/CategoryRepository.java
+++ BBDD/src/main/java/com/example/demo/entities/CategoryRepository.java
@@ -1,10 +1,12 @@
 package com.example.demo.entities;
 
+import java.util.List;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 
-   // Page<Event> findByNameCategory(String nameCategory, Pageable page);
+   List<Category> findByNameCategory(String nameCategory);
 }
